1f1f68771c302ed3d523fe0009d6457558e8db0d
Tygore <12038641+tygore587@users.noreply.github.com>
2024-12-14 16:32:55 +0100

    Adjust template

--- src/main/kotlin/day00/Solution.kt
+++ src/main/kotlin/day00/Solution.kt
@@ -5,6 +5,8 @@ import java.io.File
 
 
 private const val TestRun = true
+private const val ExpectedTestValuePart1 = -1
+private const val ExpectedTestValuePart2 = -1
 private const val Day = "00"
 
 private fun computePart1(input: List<String>): Number {
@@ -25,11 +27,19 @@ fun main() {
 
     val solutionPart1 = computePart1(input)
 
-    if (solutionPart1 != 0)
-        println("Part 1: $solutionPart1")
+    printOutput(1, solutionPart1, ExpectedTestValuePart1)
 
     val solutionPart2 = computePart2(input)
 
-    if (solutionPart2 != 0)
-        println("Part 2: $solutionPart2")
+    printOutput(2, solutionPart2, ExpectedTestValuePart2)
+}
+
+private fun printOutput(part: Number, actualSolution: Number, expectedSolution: Number) {
+    if (!TestRun) {
+        println("Solution Part $part: $actualSolution")
+    } else if (actualSolution == expectedSolution) {
+        println("Part $part works!")
+    } else {
+        println("Part $part: Wrong value. Value was: $actualSolution | Expected $expectedSolution")
+    }
 }
